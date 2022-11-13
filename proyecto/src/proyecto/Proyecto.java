0152e5d09cd26f3686ba77468ac1f3307cf97b03
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2022-11-12 20:21:21 -0600

    Add files via upload

---
+++ proyecto/src/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+/**
+ *
+ * @author Maverick Madrigal, Estefani Valverde, Andrey Salamanca, Darryl Martinez
+ */
+public class Proyecto {
+
+    /**
+     * Main del proyecto
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
