e78248cb7ae0de08582daace936b7c37dca5009b
Kevin Salazar <80077213+Kevin-Salazar-itcr@users.noreply.github.com>
2022-11-27 07:50:01 -0600

    Lectura de imagenes con OCR
    la api tess4j depende de todo el monton de apis que se agregaron, y da problemas muy tontos xd

---
+++ proyecto/src/dao/lectorOCR.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import net.sourceforge.tess4j.Tesseract;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.TesseractException;
+/**
+ *
+ * @author Andrey Salamanca, Estefani Valverde, Maverick Madrigal
+ */
+public class lectorOCR {
+   public String lectura;
+   
+   public lectorOCR() throws IOException{
+       lectura = leer("F:/foto.png");
+       System.out.println(lectura);
+   }
+   
+   public String leer(String img){
+       ITesseract t = new Tesseract();
+       try {
+           return t.doOCR(new File(img));
+       } catch (TesseractException ex) {
+           ex.printStackTrace();
+       }return "";
+   }
+   
+   public static void main(String[] args) throws IOException{
+       lectorOCR l = new lectorOCR();
+   }
+}
